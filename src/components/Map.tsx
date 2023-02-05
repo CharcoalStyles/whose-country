b94e2ccc5dd492b20e624ea7407e51c587f4b4e3
Aaron Styles <aaron@charcoalstyles.com>
2023-02-05 21:03:41 +1100

    Added working on click and zoom

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -1,9 +1,11 @@
-import { MapContainer, TileLayer, GeoJSON } from "react-leaflet";
+import { MapContainer, TileLayer, GeoJSON, Popup } from "react-leaflet";
 import { LeafletMouseEvent, Map } from "leaflet";
 import { useEffect, useRef, useState } from "react";
 import { firstNationsLocations } from "../firstNationsLocations";
 import "./Map.css";
+import { point } from "@turf/helpers";
 import bbox from "@turf/bbox";
+import booleanPointInPolygon from "@turf/boolean-point-in-polygon";
 
 type LeafletMapProps = {
   visible: boolean;
@@ -12,23 +14,45 @@ type LeafletMapProps = {
 
 const LeafletMap = ({ visible, onPoint }: LeafletMapProps) => {
   const [geojsonFocus, setGeojsonFocus] = useState<
-    GeoJSON.Feature | GeoJSON.FeatureCollection | undefined
+    GeoJSON.FeatureCollection | undefined
   >();
-  const [bounds, setBounds] = useState<[[number, number], [number, number]]>([
-    [0, 0],
-    [0, 0],
-  ]);
+  const [minZoom, setMinZoom] = useState<number>(0);
+  const [locatedPoint, setLocatedPoint] = useState<{
+    lat: number;
+    lng: number;
+  }>();
 
   useEffect(() => {
     if (visible) {
       setGeojsonFocus(firstNationsLocations);
+      const thisMap = map.current;
+      if (thisMap) {
+        setTimeout(() => {
+          setMinZoom(thisMap.getZoom());
+        }, 1);
+      }
     }
   }, [visible]);
 
   const map = useRef<Map>();
 
-  const onClick = (e: LeafletMouseEvent) => {
-    console.log({ e });
+  const onClick = ({ latlng }: LeafletMouseEvent) => {
+    const selectedFeatures = firstNationsLocations.features.filter(
+      (feature) => {
+        const polygon = feature.geometry as GeoJSON.Polygon;
+        const p = point([latlng.lng, latlng.lat]);
+        return booleanPointInPolygon(p, polygon);
+      }
+    );
+    setGeojsonFocus({
+      type: "FeatureCollection",
+      features: selectedFeatures,
+    });
+    setLocatedPoint(
+      selectedFeatures.length > 0
+        ? { lat: latlng.lat, lng: latlng.lng }
+        : undefined
+    );
   };
 
   useEffect(() => {
@@ -49,7 +73,7 @@ const LeafletMap = ({ visible, onPoint }: LeafletMapProps) => {
 
   useEffect(() => {
     const thisMap = map.current;
-    if (thisMap && geojsonFocus) {
+    if (thisMap && geojsonFocus && geojsonFocus.features.length > 0) {
       thisMap.invalidateSize();
       const bb = bbox(geojsonFocus);
       thisMap.fitBounds(
@@ -65,17 +89,67 @@ const LeafletMap = ({ visible, onPoint }: LeafletMapProps) => {
   return (
     <MapContainer
       className="map"
-      bounds={bounds}
-      zoom={6}
+      maxZoom={12}
+      minZoom={minZoom}
       ref={(mapRef) => {
         map.current = mapRef ? mapRef : undefined;
       }}
     >
       <TileLayer
-        url="https://a.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+        // url="https://a.basemaps.cartocdn.com/rastertiles/voyager_nolabels/{z}/{x}/{y}.png"
+        url="https://services.ga.gov.au/gis/rest/services/World_Bathymetry_Imagery/MapServer/tile/{z}/{y}/{x}?blankTile=false"
+        attribution='Basemap: &copy; <a href="https://www.ga.gov.au/">Commonwealth of Australia (Geoscience Australia) 2016</a>'
+        maxZoom={12}
       />
-      <GeoJSON data={firstNationsLocations} />
+
+      <GeoJSON
+        data={firstNationsLocations}
+        style={(feature) => {
+          if (
+            geojsonFocus &&
+            geojsonFocus.features.length !==
+              firstNationsLocations.features.length
+          ) {
+            const p = geojsonFocus.features.find((f) => {
+              return f.properties?.ID === feature?.properties?.ID;
+            });
+            if (p) {
+              return {
+                color: "#0f0",
+                weight: 3,
+                opacity: 1,
+                fillColor: "#000",
+                fillOpacity: 0.5,
+              };
+            }
+          }
+          return {
+            color: "#000",
+            weight: 2,
+            opacity: 1,
+            fillColor: "#000",
+            fillOpacity: 0,
+          };
+        }}
+      />
+      {locatedPoint && (
+        <Popup position={[locatedPoint.lat, locatedPoint.lng]}>
+          <div>
+            <h3>Located Point</h3>
+            <p>Lat: {locatedPoint.lat}</p>
+            <p>Lng: {locatedPoint.lng}</p>
+            <ul>
+              {geojsonFocus?.features.map((feature) => {
+                return (
+                  <li key={feature.properties?.id}>
+                    {feature.properties?.Name}
+                  </li>
+                );
+              })}
+            </ul>
+          </div>
+        </Popup>
+      )}
     </MapContainer>
   );
 };
--- src/components/Splash.tsx
+++ src/components/Splash.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-import { useEffect } from "react";
 import "./Splash.css";
 
 type SplashProps = {
@@ -8,18 +7,27 @@ type SplashProps = {
 
 export const Splash = ({ onLoaded }: SplashProps) => {
   const [visible, setVisible] = React.useState(true);
-  useEffect(() => {
-    console.log("Splash");
-    setTimeout(() => {
-      console.log("Splash loaded");
-      setVisible(false);
-      onLoaded();
-    }, 1000);
-  }, [onLoaded]);
 
   return visible ? (
-    <div className="splash">
-      <h1>Hi</h1>
+    <div
+      className="splash"
+      onClick={() => {
+        onLoaded();
+        setVisible(false);
+      }}
+    >
+      <div className="content">
+        <h1>Whose Country?</h1>
+        <p>
+          This app aims to show the traditional territories of Aboriginal and
+          Torres Straight Islanders.
+        </p>
+        <p>
+          The data presented does not represent or intend to represent official
+          or legal boundaries of any Indigenous nations. To learn about
+          definitive boundaries, contact the nations in question.
+        </p>
+      </div>
     </div>
   ) : null;
 };
